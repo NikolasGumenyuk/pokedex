9742927a4a728e59361bdc156a4fab589d7effa1
NikolasGumenyuk <nikos220599@gmail.com>
2023-02-03 12:09:20 +0200

    refactoring

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
 
 import { LoginProps } from 'models/LoginProps';
 import { verifyUser } from 'service/verifyUser';
@@ -10,14 +9,12 @@ import { setUser } from 'store/UserSlice';
 
 const Login = () => {
   const { register, handleSubmit } = useForm<LoginProps>();
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   const onSubmit = async (data: LoginProps) => {
     try {
       const res = await verifyUser(data);
       dispatch(setUser(res));
-      navigate('/');
     } catch (error) {
       console.log(error);
     }
