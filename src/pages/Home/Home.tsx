95daf48cc77c2709932660b6c594d81d9f1c3c5d
NikolasGumenyuk <nikos220599@gmail.com>
2023-02-03 17:49:35 +0200

    add log out button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import { useAppDispatch } from 'store/hooks';
+import { setUser } from 'store/UserSlice';
+
 const Home: React.FC = () => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogOut = () => {
+    dispatch(setUser({ firstName: '', lastName: '', isAuth: false }));
+  };
 
   return (
     <div>
       <div>HomePage</div>
       <button onClick={() => navigate('/login')}>go to login</button>
+      <button onClick={() => handleLogOut()}>log out</button>
     </div>
   );
 };
